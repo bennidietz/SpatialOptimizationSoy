fb2ea5320c2b2fba249ad59591622aa095535776
Benni Dietz <b_diet05@uni-muenster.de>
2020-12-30 16:46:00 +0100

    Format script so that it checks for existence of files before processing

--- data/read_data.py
+++ data/read_data.py
@@ -7,113 +7,151 @@ import os
 
 default_directory = os.path.dirname(os.path.realpath(__file__))
 
-# read data from tiff file as numpy array
-landuse_original = plt.imread(default_directory + "/mt_2017_v3_1_reprojection.tif")
+filename_base_landuse_data = default_directory + "/mt_2017_v3_1_reprojection.tif"
+filename_reclass_whole = default_directory + "/MatoGrosso_2017_reclass.tif"
 
-# plot original landuse map
-f1, ax1 = plt.subplots(1)
-cmap1 = ListedColormap(["#b3cc33","#be94e8","#10773e","#eeefce","#e4a540","#a4507d","#c948a2","#be5b1d", "#f09cde","#877712","#614040","#1b5ee4", "#0cf8c1","#00000000","#00000000"])
-legend_landuse1 = [
-        mpatches.Patch(color="#be94e8", label = 'Fallow/cotton'),
-        mpatches.Patch(color="#10773e",label = 'Forest'),
-        mpatches.Patch(color="#eeefce",label = 'Pasture'),
-        mpatches.Patch(color="#e4a540",label = 'Soy/corn'),
-        mpatches.Patch(color="#a4507d",label = 'Soy/cotton'),
-        mpatches.Patch(color="#c948a2",label = 'Soy/fallow'),
-        mpatches.Patch(color="#be5b1d",label = 'Soy/millet'),
-        mpatches.Patch(color="#f09cde",label = 'Soy/sunflower'),
-        mpatches.Patch(color="#877712",label = 'Sugarcane'),
-        mpatches.Patch(color="#614040",label = 'Urban area'),
-        mpatches.Patch(color="#1b5ee4",label = 'Water'),
-        mpatches.Patch(color="#0cf8c1",label = 'Secondary veg.'),
-        mpatches.Patch(color="#00000000",label = 'No data')
-]
-im1 = plt.imshow(landuse_original,interpolation='none',
-cmap=cmap1,vmin = 0.5, vmax = 15.5)
-ax1.set_title('Landuse map original')
-ax1.set_xlabel('Column #')
-ax1.set_ylabel('Row #')
-ax1.legend(handles=legend_landuse1,bbox_to_anchor=(1.05,1), loc=2,
-    borderaxespad=0.)
-plt.imsave(default_directory +
-    "/MatoGrosso_2017_original.tif",
-    landuse_original,format='tiff',cmap=cmap1)
+filename_reclass_amazon_tif = default_directory + "/Reclassified_Crop_Amazon.tif"
+filename_reclass_amazon_npy = default_directory + "/landuse_map_in_amazon.npy"
+filename_reclass_cerrado_tif = default_directory + "/Reclassified_Crop_Cerrado.tif"
+filename_reclass_cerrado_npy = default_directory + "/landuse_map_in_cerrado.npy"
 
-# plot reclassified landuse map
-f2, ax2 = plt.subplots(1)
-cmap2 = ListedColormap(["#b3cc33","#be94e8","#1b5ee4", "#10773e", "#000000"])
-legend_landuse2 = [
-        mpatches.Patch(color="#b3cc33",label = 'Soy'),
-        mpatches.Patch(color="#be94e8",label = 'Urban areas and infrastructure'),
-        mpatches.Patch(color="#1b5ee4",label = 'Water'),
-        mpatches.Patch(color="#10773e",label = 'Not soy'),
-        mpatches.Patch(color="#000000",label = 'No data')
-]
+filename_base_soy_yield = default_directory + "/soy_new.asc"
+filename_soy_amazon = default_directory + "/soy_potential_yield_amazon.pkl"
+filename_soy_cerrado = default_directory + "/soy_potential_yield_cerrado.pkl"
 
-cmap3 = ListedColormap(["#b3cc33","#be94e8","#1b5ee4", "#10773e"])
-legend_landuse3 = [
-        mpatches.Patch(color="#b3cc33",label = 'Soy'),
-        mpatches.Patch(color="#be94e8",label = 'Urban areas and infrastructure'),
-        mpatches.Patch(color="#1b5ee4",label = 'Water'),
-        mpatches.Patch(color="#10773e",label = 'Not soy')
-]
+def getEnding(string):
+    return "'" + string[string.rindex("/")+1:] + "'"
 
-# create empty map
-rows = landuse_original.shape[0]
-cols = landuse_original.shape[1]
-landuse_reclass = np.zeros((rows,cols),dtype= 'uint8')
-# reclassify landuse map
-landuse_reclass[landuse_original == 1] = 4
-landuse_reclass[landuse_original == 2] = 4
-landuse_reclass[landuse_original == 3] = 4
-landuse_reclass[landuse_original == 4] = 4
-landuse_reclass[landuse_original == 5] = 1
-landuse_reclass[landuse_original == 6] = 1
-landuse_reclass[landuse_original == 7] = 1
-landuse_reclass[landuse_original == 8] = 1
-landuse_reclass[landuse_original == 9] = 1
-landuse_reclass[landuse_original == 10] = 4
-landuse_reclass[landuse_original == 11] = 2
-landuse_reclass[landuse_original == 12] = 3
-landuse_reclass[landuse_original == 13] = 4
-landuse_reclass[landuse_original == 15] = 5
+def printFileCreated(filename):
+    print("New file was created at: " + getEnding(filename) + "...")
 
-im1 = plt.imshow(landuse_original,interpolation='none',
-cmap=cmap2,vmin = 0.5, vmax = 15.5)
-ax2.set_title('Landuse map reclassified')
-ax2.set_xlabel('Column #')
-ax2.set_ylabel('Row #')
-ax2.legend(handles=legend_landuse3,bbox_to_anchor=(1.05,1), loc=2,
-    borderaxespad=0.)
-plt.imsave(default_directory + "/MatoGrosso_2017_reclass.tif", landuse_reclass, format = "tiff", cmap = cmap2)
+def printFileAlreadyExists(filename):
+    print("The file " + getEnding(filename) + " already exists.")
 
-amazon_crop = landuse_reclass[900:1500,600:1600]
-plt.imsave(default_directory +
-    "/Reclassified_Crop_Amazon.tif",
-    amazon_crop,format='tiff',cmap=cmap3)
-np.save(default_directory + "/landuse_map_in_amazon.npy",amazon_crop)
+def allClassifiedRastersExist():
+    return os.path.isfile(filename_reclass_whole) \
+    and os.path.isfile(filename_reclass_amazon_npy) and os.path.isfile(filename_reclass_amazon_tif) \
+    and os.path.isfile(filename_reclass_cerrado_npy) and os.path.isfile(filename_reclass_cerrado_tif)
 
-cerrado_crop = landuse_reclass[3700:4300,4000:5000]
-plt.imsave(default_directory +
-    "/Reclassified_Crop_Cerrado.tif",
-    cerrado_crop,format='tiff',cmap=cmap3)
-np.save(default_directory + "/landuse_map_in_cerrado.npy",cerrado_crop)
+if os.path.isfile(filename_base_landuse_data):
+    if not allClassifiedRastersExist():
+        # read data from tiff file as numpy array
+        landuse_original = plt.imread(filename_base_landuse_data)
 
-plt.show()
+        # plot original landuse map
+        f1, ax1 = plt.subplots(1)
+        cmap1 = ListedColormap(["#b3cc33","#be94e8","#10773e","#eeefce","#e4a540","#a4507d","#c948a2","#be5b1d", "#f09cde","#877712","#614040","#1b5ee4", "#0cf8c1","#00000000","#00000000"])
+        legend_landuse1 = [
+                mpatches.Patch(color="#be94e8", label = 'Fallow/cotton'),
+                mpatches.Patch(color="#10773e",label = 'Forest'),
+                mpatches.Patch(color="#eeefce",label = 'Pasture'),
+                mpatches.Patch(color="#e4a540",label = 'Soy/corn'),
+                mpatches.Patch(color="#a4507d",label = 'Soy/cotton'),
+                mpatches.Patch(color="#c948a2",label = 'Soy/fallow'),
+                mpatches.Patch(color="#be5b1d",label = 'Soy/millet'),
+                mpatches.Patch(color="#f09cde",label = 'Soy/sunflower'),
+                mpatches.Patch(color="#877712",label = 'Sugarcane'),
+                mpatches.Patch(color="#614040",label = 'Urban area'),
+                mpatches.Patch(color="#1b5ee4",label = 'Water'),
+                mpatches.Patch(color="#0cf8c1",label = 'Secondary veg.'),
+                mpatches.Patch(color="#00000000",label = 'No data')
+        ]
+        im1 = plt.imshow(landuse_original,interpolation='none',
+        cmap=cmap1,vmin = 0.5, vmax = 15.5)
+        ax1.set_title('Landuse map original')
+        ax1.set_xlabel('Column #')
+        ax1.set_ylabel('Row #')
+        ax1.legend(handles=legend_landuse1,bbox_to_anchor=(1.05,1), loc=2,
+            borderaxespad=0.)
+        plt.imsave(default_directory +
+            "/MatoGrosso_2017_original.tif",
+            landuse_original,format='tiff',cmap=cmap1)
 
-soy_pot_yield_amazon = np.loadtxt(default_directory + 
-    "/soy_new.asc", 
-    skiprows=6)[900:1500,600:1600]
+        # plot reclassified landuse map
+        f2, ax2 = plt.subplots(1)
+        cmap2 = ListedColormap(["#b3cc33","#be94e8","#1b5ee4", "#10773e", "#000000"])
+        legend_landuse2 = [
+                mpatches.Patch(color="#b3cc33",label = 'Soy'),
+                mpatches.Patch(color="#be94e8",label = 'Urban areas and infrastructure'),
+                mpatches.Patch(color="#1b5ee4",label = 'Water'),
+                mpatches.Patch(color="#10773e",label = 'Not soy'),
+                mpatches.Patch(color="#000000",label = 'No data')
+        ]
 
-with open(default_directory + "/soy_potential_yield_amazon.pkl", 'wb') as output:
-    pickle.dump(soy_pot_yield_amazon, output, pickle.HIGHEST_PROTOCOL)
+        cmap3 = ListedColormap(["#b3cc33","#be94e8","#1b5ee4", "#10773e"])
+        legend_landuse3 = [
+                mpatches.Patch(color="#b3cc33",label = 'Soy'),
+                mpatches.Patch(color="#be94e8",label = 'Urban areas and infrastructure'),
+                mpatches.Patch(color="#1b5ee4",label = 'Water'),
+                mpatches.Patch(color="#10773e",label = 'Not soy')
+        ]
 
-soy_pot_yield_cerrado = np.loadtxt(default_directory + 
-    "/soy_new.asc", 
-    skiprows=6)[3700:4300,4000:5000]
+        # create empty map
+        rows = landuse_original.shape[0]
+        cols = landuse_original.shape[1]
+        landuse_reclass = np.zeros((rows,cols),dtype= 'uint8')
+        # reclassify landuse map
+        landuse_reclass[landuse_original == 1] = 4
+        landuse_reclass[landuse_original == 2] = 4
+        landuse_reclass[landuse_original == 3] = 4
+        landuse_reclass[landuse_original == 4] = 4
+        landuse_reclass[landuse_original == 5] = 1
+        landuse_reclass[landuse_original == 6] = 1
+        landuse_reclass[landuse_original == 7] = 1
+        landuse_reclass[landuse_original == 8] = 1
+        landuse_reclass[landuse_original == 9] = 1
+        landuse_reclass[landuse_original == 10] = 4
+        landuse_reclass[landuse_original == 11] = 2
+        landuse_reclass[landuse_original == 12] = 3
+        landuse_reclass[landuse_original == 13] = 4
+        landuse_reclass[landuse_original == 15] = 5
 
-with open(default_directory + "/soy_potential_yield_cerrado.pkl", 'wb') as output:
-    pickle.dump(soy_pot_yield_cerrado, output, pickle.HIGHEST_PROTOCOL)
+        im1 = plt.imshow(landuse_original,interpolation='none',
+        cmap=cmap2,vmin = 0.5, vmax = 15.5)
+        ax2.set_title('Landuse map reclassified')
+        ax2.set_xlabel('Column #')
+        ax2.set_ylabel('Row #')
+        ax2.legend(handles=legend_landuse3,bbox_to_anchor=(1.05,1), loc=2,
+            borderaxespad=0.)
+        plt.imsave(filename_reclass_whole, landuse_reclass, format = "tiff", cmap = cmap2)
+
+        amazon_crop = landuse_reclass[900:1500,600:1600]
+        plt.imsave(filename_reclass_amazon_tif,amazon_crop,format='tiff',cmap=cmap3)
+        printFileCreated(filename_reclass_amazon_tif)
+        np.save(filename_reclass_amazon_npy,amazon_crop)
+        printFileCreated(filename_reclass_amazon_npy)
+
+        cerrado_crop = landuse_reclass[3700:4300,4000:5000]
+        plt.imsave(filename_reclass_cerrado_tif,cerrado_crop,format='tiff',cmap=cmap3)
+        printFileCreated(filename_reclass_cerrado_tif)
+        np.save(filename_reclass_cerrado_npy,cerrado_crop)
+        printFileCreated(filename_reclass_cerrado_npy)
+
+        plt.show()
+    else: 
+        print("The reclassified rasters already exists for amazon and cerrado.")
+elif allClassifiedRastersExist():
+    print("The reclassified rasters already exists for amazon and cerrado.")
+else: 
+    print("To create the classified rasters for amazon and cerado, the file " + getEnding(filename_base_landuse_data) + " is required.")
+
+if os.path.exists(filename_base_soy_yield):
+    if os.path.exists(filename_soy_amazon):
+        printFileAlreadyExists(filename_soy_amazon)
+    else:
+        soy_pot_yield_amazon = np.loadtxt(filename_base_soy_yield, skiprows=6)[900:1500,600:1600]
+        with open(filename_soy_amazon, 'wb') as output:
+            pickle.dump(soy_pot_yield_amazon, output, pickle.HIGHEST_PROTOCOL)
+        printFileCreated(filename_soy_amazon)
+    if os.path.exists(filename_soy_cerrado):
+        printFileAlreadyExists(filename_soy_cerrado)
+    else:
+        soy_pot_yield_cerrado = np.loadtxt(filename_base_soy_yield,skiprows=6)[3700:4300,4000:5000]
+        with open(filename_soy_cerrado, 'wb') as output:
+            pickle.dump(soy_pot_yield_cerrado, output, pickle.HIGHEST_PROTOCOL)
+        printFileCreated(filename_soy_cerrado)
+elif not os.path.isfile(filename_soy_amazon) or not os.path.isfile(filename_soy_cerrado):
+    print("The file " + getEnding(filename_base_soy_yield) + " is required to generate the data for amazon and cerrado.")
 
 '''
 # read potential yield maps from asc file
