e987d0e075f74d9c194222ef6f398c23b0e1168b
Christian Terbeck <mail@christian-terbeck.de>
2021-01-04 14:51:35 +0100

    improved nc reading script - cropping works now

--- data/readnetcdf.py
+++ data/readnetcdf.py
@@ -12,16 +12,28 @@ filename = "prec_monthly_UT_Brazil_v2_198001_201312.nc"
 file = default_directory + "/" + filename
 
 data = Dataset(file, mode='r')
+#print(data)
 #print(data.variables)
 
 #crop dataset => precipitation subset
 
-print(data.variables['prec'].getValue())
+print(data.variables['latitude'][:])
+print(data.variables['longitude'][:])
+#print(data.variables['prec'][:])
 #print(dir(data.variables['latitude']))
 #print(data.variables['latitude'].dimensions())
 
-latbounds = [-8.954522, -52.19710]
-lonbounds = [-60.530319, -15.39383]
+#limitsLat = [-35, 7]
+#limitsLon = [-75, -31]
+
+#first bounding box: -8.954522, -60.530319 -> -10.090870, -58.636405
+#second bounding box: -14.25748, -54.09101 -> -15.39383, -52.19710
+
+latbounds = [-10.090970, -8.954522]
+lonbounds = [-60.530319, -58.636405]
+
+#latbounds = [-15.39383, -14.25748]
+#lonbounds = [-54.09101, -52.19710]
 
 lats = data.variables['latitude'][:]
 lons = data.variables['longitude'][:]
@@ -34,5 +46,5 @@ latui = np.argmin(np.abs(lats - latbounds[1]))
 lonli = np.argmin(np.abs(lons - lonbounds[0]))
 lonui = np.argmin(np.abs(lons - lonbounds[1]))
 
-precSubset = data.variables['prec'][: ,latli:latui , lonli:lonui]
-#print(precSubset)
+precSubset = data.variables['prec'][:,latli:latui, lonli:lonui]
+print(precSubset)
