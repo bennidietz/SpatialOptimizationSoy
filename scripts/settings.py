c9e45d47d4a4d0cd47003a1f9dd862cc9a8fca09
Benni Dietz <b_diet05@uni-muenster.de>
2021-01-09 09:41:14 +0100

    Adjusted crop coordinates in settings

--- scripts/settings.py
+++ scripts/settings.py
@@ -28,14 +28,18 @@ file_tmax = "Tmax_daily_UT_Brazil_v2_20140101_20170731_s1.nc"
 
 #bounding boxes
 
-bounding_box_amazon_lat_bounds = [-10.090970, -8.954522]
+bounding_box_amazon_lat_bounds = [-10.46965, -9.712087]
+#bounding_box_amazon_lat_bounds = [-10.090970, -8.954522]
 #bounding_box_amazon_lat_bounds = [-10.56435, -9.712087]
-bounding_box_amazon_lon_bounds = [-60.530319, -58.636405]
+bounding_box_amazon_lon_bounds = [-56.458405, -55.70084]
+#bounding_box_amazon_lon_bounds = [-60.530319, -58.636405]
 #bounding_box_amazon_lon_bounds = [-56.458405, -55.60614]
 
-bounding_box_cerrado_lat_bounds = [-15.39383, -14.25748]
+bounding_box_cerrado_lat_bounds = [-15.01504, -14.25748]
+#bounding_box_cerrado_lat_bounds = [-15.39383, -14.25748]
 #bounding_box_cerrado_lat_bounds = [-16.62487, -15.77261]
-bounding_box_cerrado_lon_bounds = [-54.09101, -52.19710]
+bounding_box_cerrado_lon_bounds = [-54.09101, -53.33345]
+#bounding_box_cerrado_lon_bounds = [-54.09101, -52.19710]
 #bounding_box_cerrado_lon_bounds = [-57.87884, -57.02658]
 
 #return functions
