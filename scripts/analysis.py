0d90412a53aa1a1247a6ef7f151b8c2fc0c4e98a
Christian Terbeck <mail@christian-terbeck.de>
2021-02-07 15:01:59 +0100

    adjusted color map in analysis script

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 maps = initial_population.initialize_spatial(3, settings.get_file_reclass_amazon_npy())
-f, axes = plt.subplots(1,3)
+f, axes = plt.subplots(1, 3)
 
-cmap = ListedColormap(["#b3cc33","#be94e8","#1b5ee4", "#10773e"])
+cmap = ListedColormap(["#b3cc33", "#10773e", "#be94e8", "#1b5ee4"])
 
 for amap, ax in zip(maps, axes):
-    im = ax.imshow(amap,interpolation='none', cmap=cmap,vmin = 0.5, vmax = 4.5)
+    im = ax.imshow(amap,interpolation='none', cmap=cmap, vmin = 0.5, vmax = 4.5)
 
 plt.colorbar(im, orientation='horizontal')
 plt.show()
